ba169101bcbd5a92f7ac7a8f6c05998722d7cda6
Nesss86 <Nessymonster86@gmail.com>
2025-02-02 18:53:59 -0500

    Fix price formatting issues using Money object and helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def format_price(amount)
+    # Convert integers to Money objects before formatting
+    money = amount.is_a?(Integer) ? Money.new(amount, "USD") : amount
+    money.format(symbol: true, no_cents_if_whole: false)
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
